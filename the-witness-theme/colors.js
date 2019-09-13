241f9b625c9e8aa776bcbe1cf7bc751c6f61c501
James A. Sral <jamessral@gmail.com>
2019-09-13 09:11:45 -0400

    Actually fix the witness theme colors

--- the-witness-theme/colors.js
+++ the-witness-theme/colors.js
@@ -4,9 +4,9 @@ module.exports.colors = {
   "dark": {
     "shade0": "#072626",
     "shade7": "#D3B58D",
-    "accent0": "#D3B58D",
+    "accent0": "#894056",
     "accent1": "#D3B58D",
-    "accent2": "#D3B58D",
+    "accent2": "#F8DB5D",
     "accent3": "#0AB1AF",
     "accent4": "#1EB11F",
     "accent5": "#0AB1AF",
@@ -33,4 +33,4 @@ module.exports.colors = {
   }
 };
 
-// Your theme's URL: https://themer.dev/?colors.dark.accent0=%23d3b58d&colors.dark.accent1=%23d3b58d&colors.dark.accent2=%23d3b58d&colors.dark.accent3=%230AB1AF&colors.dark.accent4=%231EB11F&colors.dark.accent5=%230AB1AF&colors.dark.accent6=%2373EC82&colors.dark.accent7=%23d3b58d&colors.dark.shade0=%23072626&colors.dark.shade1=%23393e48&colors.dark.shade2=%234b515c&colors.dark.shade3=%235c6370&colors.dark.shade4=%23636d83&colors.dark.shade5=%23828997&colors.dark.shade6=%23979eab&colors.dark.shade7=%23d3b58d&colors.light.accent0=%23e45649&colors.light.accent1=%23986801&colors.light.accent2=%23c18401&colors.light.accent3=%2350a14f&colors.light.accent4=%230184bc&colors.light.accent5=%234078f2&colors.light.accent6=%23a626a4&colors.light.accent7=%23ca1243&colors.light.shade0=%23fafafa&colors.light.shade1=%23CDCED1&colors.light.shade2=%23a0a1a7&colors.light.shade3=%239d9d9f&colors.light.shade4=%2383858B&colors.light.shade5=%23696c77&colors.light.shade6=%2351535D&colors.light.shade7=%23383a42&activeColorSet=dark&calculateIntermediaryShades.dark=true&calculateIntermediaryShades.light=false
+// Your theme's URL: https://themer.dev/?colors.dark.accent0=%23894056&colors.dark.accent1=%23d3b58d&colors.dark.accent2=%23f8db5d&colors.dark.accent3=%230AB1AF&colors.dark.accent4=%231EB11F&colors.dark.accent5=%230AB1AF&colors.dark.accent6=%2373EC82&colors.dark.accent7=%23d3b58d&colors.dark.shade0=%23072626&colors.dark.shade1=%23393e48&colors.dark.shade2=%234b515c&colors.dark.shade3=%235c6370&colors.dark.shade4=%23636d83&colors.dark.shade5=%23828997&colors.dark.shade6=%23979eab&colors.dark.shade7=%23d3b58d&colors.light.accent0=%23e45649&colors.light.accent1=%23986801&colors.light.accent2=%23c18401&colors.light.accent3=%2350a14f&colors.light.accent4=%230184bc&colors.light.accent5=%234078f2&colors.light.accent6=%23a626a4&colors.light.accent7=%23ca1243&colors.light.shade0=%23fafafa&colors.light.shade1=%23CDCED1&colors.light.shade2=%23a0a1a7&colors.light.shade3=%239d9d9f&colors.light.shade4=%2383858B&colors.light.shade5=%23696c77&colors.light.shade6=%2351535D&colors.light.shade7=%23383a42&activeColorSet=dark&calculateIntermediaryShades.dark=true&calculateIntermediaryShades.light=false
