9d389fdc516a325d51df697441471fe2bbe6ce99
James A. Sral <jamessral@gmail.com>
2023-05-25 10:24:50 -0400

    Add wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,13 +7,16 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'duskfox'
+config.color_scheme = 'Night Owlish Light'
+config.colors = { cursor_fg = 'rgba(200,200,200,0.9)' }
+
+
 config.font = wezterm.font 'Liberation Mono'
 config.font_size = 15
 
